e83ded8c8ddf21f3e18bacd32369da68b27f2b3f
root <unvisitedplanets@gmail.com>
2020-05-22 18:24:17 +0530

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -17,7 +17,15 @@ app.use('/wiki', wiki);
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://megatron:prome123@decepticons-psc3o.mongodb.net/local_library?retryWrites=true&w=majority';
+
+
+
+// Set up mongoose connection
+
+var dev_db_url = 'mongodb+srv://megatron:prome123@decepticons-psc3o.mongodb.net/local_library?retryWrites=true&w=majority';
+
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
